ef4eb6d1dfa697de9af0d4174e0e4b7937fdf6f9
dsabelli <dsabelli3@gmail.com>
2024-08-20 09:50:33 -0400

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,4 @@
 // vite.config.js
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  build: {
-    outDir: "netlify", // Output directory for the build
-    assetsDir: "", // No subdirectory for assets
-    rollupOptions: {
-      input: "/src/pages/index.html", // Entry point
-    },
-  },
-});
+export default defineConfig({});
