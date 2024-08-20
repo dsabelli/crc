402beca68844c8cdc58d13469fdef9dbececd780
dsabelli <dsabelli3@gmail.com>
2024-08-20 09:26:58 -0400

    change build output

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
+// vite.config.js
 import { defineConfig } from "vite";
+
 export default defineConfig({
   build: {
+    outDir: "netlify", // Output directory for the build
+    assetsDir: "", // No subdirectory for assets
     rollupOptions: {
-      input: {
-        app: "./src/pages/index.html",
-      },
+      input: "/src/pages/index.html", // Entry point
     },
-    outDir: "..",
   },
 });
